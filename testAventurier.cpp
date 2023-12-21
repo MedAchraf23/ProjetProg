34120df7b9fba04657e9c7d5c77eb8fe08abf65a
Reda Dali <reda.dali68@gmail.com>
2023-12-21 18:33:43 +0100

    Tests unitaire concernant la class aventurier

---
+++ testAventurier.cpp
@@ -0,0 +1,20 @@
+#include "doctest.h"
+#include "aventurier.h"
+
+TEST_CASE("La class aventurier fonctionne correctement") {
+
+    SUBCASE("Le getVie fonctionne correctement") {
+        int vie = 10;
+        aventurier a{vie};
+        REQUIRE_EQ(a.getVie(),vie);
+    }
+
+    SUBCASE("Le choix du deplacment est bien calcule") {
+        int vie = 2, force 3, y = 11, x = 10;
+        aventurier a{vie,force,x,y};
+        int choix = 1;
+        a.deplacer(choix);
+        REQUIRE_EQ(a.getX(),x+1);
+        REQUIRE_EQ(a.getY(),y);
+    }
+}
