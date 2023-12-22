e53acc482df512911fab35b9f409cf48973c88e8
Kader Chaouchi <kader.chaouch@hotmail.com>
2023-12-22 01:13:20 +0100

    Ajout deplacements : Kader

--- monstreAveugle.h
+++ monstreAveugle.h
@@ -5,12 +5,17 @@
 #ifndef PROJETQUALITEPROG_MONSTREAVEUGLE_H
 #define PROJETQUALITEPROG_MONSTREAVEUGLE_H
 
+#include "aventurier.h"
 #include "monstre.h"
+#include "ctime"
+#include "random"
 
 class monstreAveugle : public monstre {
 public:
     monstreAveugle(int ptsVie, int ptsForce, int habilete, int x, int y);
-    void seDeplace() override;
+    void seDeplace();
+
+
 
 
 
