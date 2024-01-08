f80b7de37b286e0ec32d588301e4ebf3b92613bc
Reda Dali <reda.dali68@gmail.com>
2024-01-09 00:25:01 +0100

    Ajout

--- affichage.cpp
+++ affichage.cpp
@@ -1,63 +1,84 @@
-//#include "affichage.h"
-//
-//
-//
-//affichage::affichage(const ostream& ost, const aventurier& aventurier, const terrain& terrain, const monstre& monstre) :
-//    d_ost{ost}, d_aventurier{aventurier}, d_terrain{terrain}, d_monstre{monstre}
-//{
-//
-//}
-//
-//aventurier affichage::getAventurier() const
-//{
-//    return d_aventurier;
-//}
-//
-//terrain affichage::getTerrain() const
-//{
-//    return d_terrain;
-//}
-//
-//monstre affichage::getMonstre() const
-//{
-//    return d_monstre;
-//}
-//
-//void affichage::afficheMenu() const
-//{
-//    int choix;
-//    do
-//    {
-//        std::cout<<"MENU PRINCIPAL :"<<'\n'<<'\n';
-//        std::cout<<"(1) Lancer la partie"<<'\n';
-//        std::cout<<"(2) Creer/editer le terrain d置n cheteau"<<'\n';
-//        std::cout<<"(3) Charger la configuration d置n terrain"<<'\n';
-//        std::cout<<std::endl<<"Votre choix : ";
-//        std::cin>>choix;
-//    }
-//    while (choix<1 || choix>3);
-//    return choix;
-//}
-//
-//void affichage::afficheTerrain() const
-//{
-//
-//}
-//
-//void affichage::afficheAventurier() const
-//{
-//
-//}
-//
-//void affichage::afficheMonstre() const
-//{
-//
-//}
-//
-//void affichage::afficheInfoAventurier() const
-//{
-//    std::cout<<"Points de vie : "<<d_aventurier.getVie()<<'/n';
-//    std::cout<<"Points de force : "<<d_aventurier.getForce()<<'/n';
-//    std::cout<<"Solidite armure : "<<d_aventurier.getArmure().getPointSolidite()<<'/n';
-//    std::cout<<"Solidite epee : "<<d_aventurier.getEpee().getPointSolidite()<<'/n';
-//}
+#include "affichage.h"
+
+
+affichage::affichage(const ostream& ost, const aventurier& aventurier, const terrain& terrain, const monstre& monstre) :
+    d_ost{ost}, d_aventurier{aventurier}, d_terrain{terrain}, d_monstre{monstre}
+{
+
+}
+
+aventurier affichage::getAventurier() const
+{
+    return d_aventurier;
+}
+
+terrain affichage::getTerrain() const
+{
+    return d_terrain;
+}
+
+monstre affichage::getMonstre() const
+{
+    return d_monstre;
+}
+
+void affichage::afficheMenu() const
+{
+    int choix;
+    do
+    {
+        std::cout<<"MENU PRINCIPAL :"<<'\n'<<'\n';
+        std::cout<<"(1) Lancer la partie"<<'\n';
+        std::cout<<"(2) Creer/editer le terrain d置n chateau"<<'\n';
+        std::cout<<"(3) Charger la configuration d置n terrain"<<'\n';
+        std::cout<<std::endl<<"Votre choix : ";
+        std::cin>>choix;
+    }
+    while (choix<1 || choix>3);
+    return choix;
+}
+
+void affichage::afficheTerrain(std::ostream& fichier) const
+{
+    if(!fichier.open())
+    {
+        cout<<"Le fichier ne s'est pas ouvert";
+    }
+    else
+    {
+        while(!fichier.eof())
+        {
+            char carac;
+            cin>>carac;
+            if(carac == '.')
+            {
+                cout<<" ";
+            }
+            else
+            if(carac == '+')
+            {
+                cout<<endl;
+            }
+            else
+            cout<<carac;
+        }
+    }
+}
+
+void affichage::afficheAventurier() const
+{
+
+}
+
+void affichage::afficheMonstre() const
+{
+
+}
+
+void affichage::afficheInfoAventurier() const
+{
+    std::cout<<"Points de vie : "<<d_aventurier.getVie()<<'/n';
+    std::cout<<"Points de force : "<<d_aventurier.getForce()<<'/n';
+    std::cout<<"Solidite armure : "<<d_aventurier.getArmure().getPointSolidite()<<'/n';
+    std::cout<<"Solidite epee : "<<d_aventurier.getEpee().getPointSolidite()<<'/n';
+}
